fe3f63301458bf482fb4da7837f9abc78d047be2
William Swaney <the2bears@gmail.com>
2016-08-22 21:13:18 -0700

    Testing another set of points for spline

--- desktop/src-common/ds3/splines.clj
+++ desktop/src-common/ds3/splines.clj
@@ -26,11 +26,14 @@
         x1 (if x-left hook-near-x hook-width-near-x)
         x2 (if x-left hook-width-near-x hook-near-x)
         x3 (if x-left 0 (c/screen-to-world c/game-width))
+        x4 (if x-left (c/screen-to-world c/game-width) 0)
         ty (- y hook-near-x)
         dy (/ (- ty bottom-y) 5)
         points [[x y][x y][x (+ y hook-near-x)][(x-fn x hook-near-x) (+ y hook-near-x)][(x-fn x hook-near-x) ty]
                 [x1 (- ty dy)][x1 (- ty (* 2 dy))][x2 (- ty (* 3 dy))][x2 (- ty (* 4 dy))][x1 bottom-y][x3 (- hook-far-x)][x3 (- hook-far-x)]]
-        points-vec (points-to-vector-2 points)
+        points2 [[x y][x y][x (+ y hook-near-x)][(x-fn x hook-near-x) (+ y hook-near-x)][(x-fn x hook-near-x) ty]
+                [x1 (- ty dy)][x1 (- ty (* 3 dy))][x2 (- ty (* 4 dy))][x4 (- hook-far-x)][x4 (- hook-far-x)]]
+        points-vec (points-to-vector-2 points2)
         spline (b-spline points-vec 3 false)]
     spline))
 
